f79b441eac16acc1f50e8cf5ae6c443bb127a68e
Joseph <joseph.loveday2@gmail.com>
2019-11-15 13:50:57 +0800

    Shift literals to flash
    and other minor changes, such as making the calculator almost work.

--- Wrist_Controller.ino
+++ Wrist_Controller.ino
@@ -28,18 +28,12 @@ const char run4[] PROGMEM = "calculator";
 
 const char set1[] PROGMEM = "deadzone";
 const char set2[] PROGMEM = "calibrate";
-const char set3[] PROGMEM = "three";
-const char set4[] PROGMEM = "four";
-
-const char dialog0[] PROGMEM = "Maximum";
-const char dialog1[] PROGMEM = "Minimum";
-const char dialog2[] PROGMEM = "V1.0\nDeveloped by Joseph Loveday";
-
+const char set3[] PROGMEM = "-";
+const char set4[] PROGMEM = "-";
 
 const char *const main[] PROGMEM = {string0,main1,main2,main3,main4,string0};
 const char *const runProgs[] PROGMEM = {string0,run1,run2,run3,run4,string0};
 const char *const settings[] PROGMEM = {string0,set1,set2,set3,set4,string0};
-const char *const dialog[] PROGMEM = {dialog0,dialog1,dialog2};
 
 int leftPot = 0, rightPot = 1, buttonPin = 2;
 int val, maxVal, minVal, maxSel, minSel, choice;
@@ -64,15 +58,13 @@ void setup() {
 void calibrate() {
 
   display.clearDisplay();
-  strcpy(dialogBuffer, (char *)pgm_read_word(&(dialog[0])));
-  drawWindow(16,16,96,48,"Calibration",dialogBuffer);
+  drawWindow(16,16,96,48,F("Calibration"),dialogBuffer);
   display.display();
   delay(2000);
   maxVal = analogRead(leftPot);
   maxSel = analogRead(rightPot);
   display.clearDisplay();
-  strcpy(dialogBuffer, (char *)pgm_read_word(&(dialog[1])));
-  drawWindow(16,16,96,48,"Calibration",dialogBuffer);
+  drawWindow(16,16,96,48,F("Calibration"),dialogBuffer);
   display.display();
   delay(2000);
   minVal = analogRead(leftPot);
@@ -201,29 +193,29 @@ int getSelection(char items[6][10]) {
 }
 
 void drawNums() {
-  display.setCursor(64,32);
+  display.setCursor(64,40);
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.print("7 8 9 D");
-  display.setCursor(64,40);
-  display.print("4 5 6 .");
+  display.print(F("7 8 9 D"));
   display.setCursor(64,48);
-  display.print("1 2 3 0");
+  display.print(F("4 5 6 ."));
+  display.setCursor(64,56);
+  display.print(F("1 2 3 0"));
 }
 
 void drawOperators() {
-  display.setCursor(64,32);
+  display.setCursor(64,40);
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.print("+ -");
-  display.setCursor(64,40);
-  display.print("* / l");
+  display.print(F("+ -"));
   display.setCursor(64,48);
-  display.print("^ √ !");
+  display.print(F("* /"));
+  display.setCursor(64,56);
+  display.print(F("^ l"));
 }
 
 char getNum() {
-  int val, sel, prevX = 63, prevY = 31;
+  int val, sel, prevX = 63, prevY = 39;
   int cycles = 0, result;
   bool button, reset = false;
 
@@ -238,24 +230,24 @@ char getNum() {
       reset = true;
     }
 
-    if ((prevX != val*12 + 63) || (prevY != sel*8 + 31)) {
-      display.drawRect(prevX, prevY, 10, 8, BLACK);
+    if ((prevX != val*12 + 63) || (prevY != sel*8 + 39)) {
+      display.drawRect(prevX, prevY, 10, 8, BLACK); // blank off the last drawn box
     }
 
     prevX = val*12 + 63;
-    prevY = sel*8 + 31;
-    display.drawRect(prevX, prevY, 10, 8, WHITE);
+    prevY = sel*8 + 39;
+    display.drawRect(prevX, prevY, 10, 8, WHITE); // draw the new box
     drawNums();
 
     display.display();
   } while (button || !reset);
 
-  while (!button && cycles < 50) {
+  while (!button && cycles < 30000) { // set delay for button held, signal end of number
     button = digitalRead(buttonPin);
     cycles++;
   }
 
-  if (cycles < 50) {
+  if (cycles < 30000) {
     if (val < 3) {
       result = 55 - sel*3 + val; // 30 + number logic (for ASCII char)
     } else if (sel == 2) {
@@ -267,6 +259,63 @@ char getNum() {
     }
   } else {
     result = '\0'; // indicate the end of a number
+  }
+  return result;
+}
+
+char getOperation() {
+  int val, sel, prevX = 63, prevY = 39;
+  int cycles = 0, result;
+  bool button, reset = false;
+
+  do {
+    button = digitalRead(buttonPin);
+    val = analogRead(leftPot);
+    sel = analogRead(rightPot);
+    val = constrain(map(val, minVal, maxVal, 0, 1), 0, 1);
+    sel = constrain(map(sel, minSel, maxSel, 0, 2), 0, 2);
+
+    if (button) {
+      reset = true;
+    }
+
+    if ((prevX != val*12 + 63) || (prevY != sel*8 + 39)) {
+      display.drawRect(prevX, prevY, 10, 8, BLACK);
+    }
+
+    prevX = val*12 + 63;
+    prevY = sel*8 + 39;
+    display.drawRect(prevX, prevY, 10, 8, WHITE);
+    drawOperators();
+
+    display.display();
+  } while (button || !reset);
+
+  while (!button && cycles < 30000) { // set delay for button held, signal end of number
+    button = digitalRead(buttonPin);
+    cycles++;
+  }
+
+  if (cycles < 30000) {
+    if (val == 0) {
+      if (sel == 0) {
+        result = '+';
+      } else if (sel == 1) {
+        result = '*';
+      } else {
+        result = '^';
+      }
+    } else {
+      if (sel == 0) {
+        result = '-';
+      } else if (sel == 1) {
+        result = '/';
+      } else {
+        result = 'l';
+      }
+    }
+  } else {
+    result = 'e'; // indicate program exit 
   }
   return result;
 }
@@ -301,12 +350,6 @@ double calculate(double num1, double num2, char operation) {
     result = num1 / num2;
   } else if (operation == '^') {
     result = pow(num1,num2);
-  } else if (operation == '!') {
-    for (int i = 1; i <= (int)num2; i++) {
-      result *= i;
-    }
-  } else if (operation == 's') {
-    result = sqrt(num2);
   } else if (operation == 'l') {
     result = log10(num1) / log10(num2);
   }
@@ -372,24 +415,24 @@ void servo2(bool deadzone) {
 
 void calculator() {
   double num1 = 0, num2 = 0, result = 0;
-  char operation;
+  char operation = '+';
   char sequence[10];
   int cycles = 0, val, sel, length = 0, power;
   bool button;
-  char inputMode = 'a'; // a = input num1, b = input num2, c = input operation
+  char inputMode = 'a'; // a = input num1, b = input num2, c = input operation, d = answer input mode
   char operationMode = 'a'; // a = inputting, b = exiting
 
-  do {
-    button = digitalRead(buttonPin);
+  display.clearDisplay();
 
-    display.clearDisplay();
+  do { // loop over the input modes
+    button = digitalRead(buttonPin);
 
     for (int i = 0; i < 10; i++) {
       sequence[i] = '\0';
     }
 
     do {
-      if (inputMode <= 'b') { // check that we're inputting a number
+      if (inputMode == 'a' || inputMode == 'b') { // check that we're inputting a number
         sequence[length] = getNum();
         if (sequence[length] == '<') {
           sequence[length] = '\0';
@@ -397,28 +440,35 @@ void calculator() {
           length--;
         } else if (sequence[length] == '\0') {
           if (inputMode == 'a') {
-            inputMode == 'b';
+            for (int i = 0; i < 10; i++) { // clear sequence for input num2
+              sequence[i] = '\0';
+            }
+            length = 0;
+            operationMode = 'b'; // kick out of the number input loop
           } else if (inputMode == 'b') {
-            inputMode == 'c';
+            length = 0;
+            operationMode = 'b';
           }
         } else {
           length++;
         }
-      } else if (inputMode == 'c') {
-        //operation == getOperation();
       }
 
       if (inputMode == 'a') {
         num1 = parseDouble(sequence);
         display.setCursor(0,0);
+        display.print("->");
+        display.setCursor(16,0);
       } else if (inputMode == 'b') {
         num2 = parseDouble(sequence);
         display.setCursor(0,10);
+        display.print("->");
+        display.setCursor(16,10);
       }
-
+      
       display.print(sequence);
       display.display();
-    } while (operationMode == 'a'); // exit number input after user holds button
+    } while (operationMode == 'a' && length < 10); // exit number input after user holds button
 
     if (inputMode == 'a') {
       String(num1,10).toCharArray(sequence, 10); // read the existing num1 into sequence
@@ -429,6 +479,7 @@ void calculator() {
       String(num2,10).toCharArray(sequence, 10); // read the existing num2 into sequence
       
       inputMode = 'c';
+      operation = getOperation();
       operationMode = 'a';
     } else if (inputMode == 'c') {
       result = calculate(num1,num2,operation); // calculate the actual result
@@ -436,7 +487,7 @@ void calculator() {
       display.print(result);
     }
 
-  } while (cycles < 20);
+  } while (operation != 'e');
 
 }
 
@@ -490,11 +541,8 @@ void loop() {
         }
         break;
       case 3:
-        for (int i = 0; i < 3; i++) {
-          strcpy(buffer[i],(char *)pgm_read_word(&(dialog[i])));
-        }
         display.clearDisplay();
-        drawWindow(8,8,112,48,"About","V1.0   Developed by Joseph Loveday");
+        drawWindow(8,8,112,48,F("About"),F("V0.2   Developed by Joseph Loveday"));
         display.display();
         delay(3000);
         break;
